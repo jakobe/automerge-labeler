af6af29434b149b93b9d27bb2d38c71e5f3b8895
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-03-24 02:16:11 +0100

    :hankey: Test query

--- dist/index.js
+++ dist/index.js
@@ -441,17 +441,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
-    return t;
-};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -539,10 +528,12 @@ function getPullRequestsWithLabels() {
         //       core.setFailed(error.message);
         //     });
         core.info("Querying...");
-        const _b = {
-            dummy: "dummy",
-            query: "repo:probot/probot"
-        }, { dummy } = _b, variables = __rest(_b, ["dummy"]);
+        // const { dummy, ...variables } = {
+        //   dummy: "dummy",
+        //   query: "repo:probot/probot"
+        // };
+        const variables = {};
+        variables["query"] = "repo:probot/probot";
         const result = yield octokit
             .graphql(`query getApprovedPullRequestsWithLabels($query: String!) {
       search(query: $query, type: ISSUE, last: 3) {
