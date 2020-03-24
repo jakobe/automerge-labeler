51cf974fbd18c88c3b0a6d51b515259691157d20
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-03-24 02:12:03 +0100

    :hankey: Test script

--- dist/index.js
+++ dist/index.js
@@ -441,6 +441,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -528,6 +539,10 @@ function getPullRequestsWithLabels() {
         //       core.setFailed(error.message);
         //     });
         core.info("Querying...");
+        const _b = {
+            dummy: "dummy",
+            query: "repo:probot/probot"
+        }, { dummy } = _b, variables = __rest(_b, ["dummy"]);
         const result = yield octokit
             .graphql(`query getApprovedPullRequestsWithLabels($query: String!) {
       search(query: $query, type: ISSUE, last: 3) {
@@ -544,7 +559,7 @@ function getPullRequestsWithLabels() {
         }
       }
     }    
-  `, { query: "repo:probot/probot" })
+  `, variables)
             .catch(error => {
             core.error(JSON.stringify(error.errors));
             core.error(error.request.variables);
